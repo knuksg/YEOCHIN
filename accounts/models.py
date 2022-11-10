f620cf55fcb43007f83d4491fcad0b3f0b2af4f5
hady1024 <hady1024@gmail.com>
2022-11-10 18:48:15 +0900

    유저기능 초안

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
+from django.conf import settings
 
 # Create your models here.
+
+
 class User(AbstractUser):
+    nickname = models.CharField(max_length=20, unique=True, null=True)
+    followings = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
     pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    intro = models.TextField(blank=True)  # 소개글
+    image = ProcessedImageField(
+        blank=True,
+        upload_to="profile/",
+        processors=[Thumbnail(300, 300)],
+        format="JPEG",
+        options={"quality": 60},
+    )
