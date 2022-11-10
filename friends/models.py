0a948e2f68766dc9c48ed2a5fc9fa2bd2959a0c5
danny7128 <danny7128@gmail.com>
2022-11-10 17:35:05 +0900

    ADD Friends [이성훈]

--- friends/models.py
+++ friends/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class Friend(models.Model):
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    start_at = models.DateField()
+    end_at = models.DateField(null=True)
+
+class Friend_Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    friend =  models.ForeignKey(Friend, on_delete=models.CASCADE)
+    
+
+
+@property 
+def created_string(self):
+    time = datetime.now(tz=timezone.utc) - self.created_at
+    if time < timedelta(minutes=1):
+        return "방금 전"
+    elif time < timedelta(hours=1):
+        return str(int(time.seconds / 60)) + "분 전"
+    elif time < timedelta(days=1):
+        return str(int(time.seconds / 3600)) + "시간 전"
+    elif time < timedelta(days=7):
+        time = datetime.now(tz=timezone.utc).date() - self.created_at.date()
+        return str(time.days) + "일 전"
+    else:
+        return False
--- friends/views.py
+++ friends/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import Friend, Friend_Comment
+from . forms import FriendForm,Friend_CommentForm
 
 # Create your views here.
+def index(request):
+    friends = Friend.objects.all()
+    context = {
+        'friends':friends
+    }
+    return render(request, "friends/index.html",context)
+
+def create(request):
+    if request.method == "POST":
+        friend_form = FriendForm(request.POST, request.FILES)
+        if friend_form.is_valid():
+            friend_form.save()
+            return redirect("friends:index")
+    else:
+        friend_form = FriendForm()
+    context = {
+        "friend_form": friend_form
+        }
+    return render(request, "friends/create.html", context)
+
+def detail(request, pk):
+    friend = Friend.objects.get(pk=pk)
+    comments = friend.friend_comment_set.all()
+    comment_form = Friend_CommentForm()
+
+    context = {
+        "friend":friend,
+        "comments":comments,
+        "comment_form":comment_form,
+    }
+    return render(request,"friends/detail.html", context)
+
+def update(request, pk):
+    friend = Friend.objects.get(pk=pk)
+    if request.method == "POST":
+        friend_from = FriendForm(request.POST, request.FILES, instance=friend)
+        if friend_from.is_valid():
+            friend_from.save()
+            return redirect("friends:detail", pk)
+    else:
+        friend_form = FriendForm(instance=friend)
+    context = {
+        "friend_form":friend_form
+    }
+    return render(request, "friends/create.html", context)
+
+def delete(request, pk):
+    friend = Friend.objects.get(pk=pk)
+    friend.delete()
+    return redirect("friends:index")
+
+def comment_create(request, pk):
+    friend = Friend.objects.get(pk=pk)
+    comment_form = Friend_CommentForm(request.POST)
+    if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.friend = friend
+        comment.save()
+
+        return redirect("friends:detail", pk)
+
+def comment_delete(request, friend_pk, comment_pk):
+    friend = Friend.objects.get(pk=friend_pk)
+    comment = Friend_Comment.objects.get(pk=comment_pk)
+    comment.delete()
+    return redirect("friends:detail", friend.pk)
