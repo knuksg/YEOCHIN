5b2218b4cf0e2c0b764e32195d31a68a0e15f717
knuksg <ksgyo0121@gmail.com>
2022-11-11 10:15:05 +0900

    feat: hotel_MTV 완료

--- hotels/views.py
+++ hotels/views.py
@@ -6,14 +6,9 @@ from django.http import JsonResponse
 # Create your views here.
 def index(request):
     hotels = Hotel.objects.order_by('rating')[:4]
-    if request.method == 'POST':
-        post_region = request.GET.get('region') or '서울'
-        region = Region.objects.get(name=post_region)
-        detail_regions = DetailRegion.objects.filter(region=region)[0]
-        return JsonResponse({
-            'detail_regions': detail_regions
-        })
+    regions = Region.objects.all()
     context = {
         'hotels': hotels,
+        'regions': regions,
     }
     return render(request, "hotels/index.html", context)
