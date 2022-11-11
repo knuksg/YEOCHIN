d9be11b893a246181ec9b07eb6a269189cf45f63
knuksg <ksgyo0121@gmail.com>
2022-11-11 09:19:06 +0900

    fix: .gitignore

--- hotels/views.py
+++ hotels/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Hotel
+from main.models import Region, DetailRegion
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "hotels/index.html")
+    hotels = Hotel.objects.order_by('rating')[:4]
+    if request.method == 'POST':
+        post_region = request.GET.get('region') or '서울'
+        region = Region.objects.get(name=post_region)
+        detail_regions = DetailRegion.objects.filter(region=region)[0]
+        return JsonResponse({
+            'detail_regions': detail_regions
+        })
+    context = {
+        'hotels': hotels,
+    }
+    return render(request, "hotels/index.html", context)
