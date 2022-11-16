e86e109d3217ee30388e113f4b88529f25113aae
hady1024 <hady1024@gmail.com>
2022-11-16 23:24:26 +0900

    유저모델 수정

--- accounts/models.py
+++ accounts/models.py
@@ -7,16 +7,27 @@ from django import forms
 from django.contrib.auth.hashers import check_password
 
 
-
 # Create your models here.
 
 
 class User(AbstractUser):
-    naver_id = models.CharField(null=True, unique=True, max_length=100)    
+    naver_id = models.CharField(null=True, unique=True, max_length=100)
     nickname = models.CharField(max_length=20, unique=True, null=True)
     followings = models.ManyToManyField(
         "self", symmetrical=False, related_name="followers"
     )
+    gender = models.CharField(
+        max_length=1,
+        null=True,
+        unique=True,
+        choices=(
+            ("f", "female"),
+            ("m", "male"),
+        ),
+    )
+    age = models.PositiveIntegerField(null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, unique=True)
+
     pass
 
 
@@ -30,17 +41,22 @@ class Profile(models.Model):
         format="JPEG",
         options={"quality": 60},
     )
-    
+
+    pass
+
+
 # 비밀번호 확인
 class CheckPasswordForm(forms.Form):
-    password = forms.CharField(widget = forms.PasswordInput())
+    password = forms.CharField(widget=forms.PasswordInput())
+
     def __init__(self, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.user = user
+
     def clean(self):
         cleaned_data = super().clean()
-        password = cleaned_data.get('password')
+        password = cleaned_data.get("password")
         confirm_password = self.user.password
         if password:
             if not check_password(password, confirm_password):
-                self.add_error('password', '비밀번호가 달라요.')
+                self.add_error("password", "비밀번호가 달라요.")
