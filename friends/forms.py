9df8836891c8ef47e5dbe5102114c90c22a29a9f
ccmmaa1030 <ccmmaa1030@gmail.com>
2022-11-16 17:29:43 +0900

    design : friends-form 디자인

--- friends/forms.py
+++ friends/forms.py
@@ -1,34 +1,53 @@
 from django import forms
 from .models import Friend, Friend_Comment
 
+
 class DateInput(forms.DateInput):
-    input_type = 'date'
+    input_type = "date"
+
 
 class FriendForm(forms.ModelForm):
-
     class Meta:
         model = Friend
-        fields = ['title', 'content','start_at', 'end_at', 'place', 'people_number','image','thumbnail']
+        fields = [
+            "title",
+            "content",
+            "start_at",
+            "end_at",
+            "place",
+            "people_number",
+            "image",
+            "thumbnail",
+        ]
         widgets = {
-            'start_at':DateInput(),
-            'end_at':DateInput(),
-            
+            "title": forms.TextInput(attrs={"placeholder": "제목을 작성해 해주세요"}),
+            "content": forms.Textarea(
+                attrs={"placeholder": "자유롭게 작성해 주세요(ex. 동행 계획, 여행 스타일 등)"}
+            ),
+            "start_at": DateInput(),
+            "end_at": DateInput(),
+            "place": forms.TextInput(attrs={"placeholder": "원하는 동행 장소를 작성해 주세요"}),
+            "people_number": forms.NumberInput(
+                attrs={"placeholder": "원하는 동행 인원을 작성해 주세요"}
+            ),
         }
         labels = {
-            'title': '제목',
-            'content': '내용',
-            'start_at': '시작날짜',
-            'end_at': '마감날짜',
-            'place': '장소',
-            'people_number': '사람수',
+            "title": "제목",
+            "content": "내용",
+            "start_at": "시작날짜",
+            "end_at": "마감날짜",
+            "place": "장소",
+            "people_number": "인원",
+            "image": "이미지",
+            "thumbnail": "썸네일",
         }
+
 
 class Friend_CommentForm(forms.ModelForm):
-
     class Meta:
         model = Friend_Comment
-        fields = ['content']
+        fields = ["content"]
 
         labels = {
-            'content' : '댓글',
+            "content": "댓글",
         }
